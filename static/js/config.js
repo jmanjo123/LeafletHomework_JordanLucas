bd46d676a29ba85a32ab50d472b4c8ce21b2871e
jmanjo123 <56357627+jmanjo123@users.noreply.github.com>
2020-05-11 00:38:20 -0400

    update

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API Key
-const API_KEY = "AIzaSyBfeXote62sE-iZuboXtdYogKsgTi8b0Sc";
+const API_KEY = "pk.eyJ1Ijoiam1hbmpvcmRhbiIsImEiOiJjazcyaXU4c2wwMnF0M2ZwbnlhMjl1azdkIn0.Dxwp7QQpQfxVX9oLgkWOXQ";
